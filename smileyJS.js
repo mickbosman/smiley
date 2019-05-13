a7883d06ade38758ef2a36fe2f84f5212bc77d2b
mickbosman <42744703+mickbosman@users.noreply.github.com>
2019-05-13 11:30:31 +0200

    Add files via upload

---
+++ smileyJS.js
@@ -0,0 +1,155 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+context.beginPath();
+context.arc(340,180,40,0,2*Math.PI);
+context.fillStyle = "#eac086";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(30,380,120,80,300*Math.PI);
+context.fillStyle = "#ffcd94";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(230,380,120,80,300*Math.PI);
+context.fillStyle = "#ffcd94";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(128,320,140,80,300*Math.PI);
+context.fillStyle = "#ffcd94";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(60,180,40,0,2*Math.PI);
+context.fillStyle = "#eac086";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(180,35,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(150,40,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(120,52,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(90,75,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(210,35,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(240,43,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(270,60,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(295,85,20,20,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(200,200,150,0,2*Math.PI);
+context.fillStyle = "#ffcd94";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(200,250,80,0,1*Math.PI);
+context.closePath();
+context.fillStyle = "lightpink";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(200,250,18,20,300*Math.PI);
+context.fillStyle = "white";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(175,250,18,20,300*Math.PI);
+context.fillStyle = "white";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(200,200,20,0,2*Math.PI);
+context.closePath();
+context.fillStyle = "red";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(270,150,35,0,2*Math.PI);
+context.fillStyle = "white";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(130,150,35,0,2*Math.PI);
+context.fillStyle = "white";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(130,160,15,0,2*Math.PI);
+context.closePath();
+context.fillStyle = "lightblue";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.arc(270,160,15,0,2*Math.PI);
+context.closePath();
+context.fillStyle = "lightblue";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(110,100,50,10,0*Math.PI);
+context.fillStyle = "#8D6645";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(240,100,50,10,0*Math.PI);
+context.fillStyle = "#8D6645";
+context.stroke();
+context.fill();
+
+context.beginPath();
+context.rect(130,220,4,4,300*Math.PI);
+context.fillStyle = "saddlebrown";
+context.stroke();
+context.fill();
